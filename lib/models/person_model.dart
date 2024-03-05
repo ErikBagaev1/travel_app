5db05a9b9c6bf0485b2f9ddeaf789571a7711590
Ergos <bagaeff.erik@yandex.ru>
2024-03-05 09:53:17 +0300

    добавил person_model

---
+++ lib/models/person_model.dart
@@ -0,0 +1,17 @@
+class Person {
+  String? firstName;
+  String? lastName;
+  String? fatherName;
+  int? countChildren;
+  int? numberPassport;
+  int? seriesPassport;
+  String? email;
+  Person(
+      {this.firstName,
+      this.lastName,
+      this.fatherName,
+      this.countChildren,
+      this.numberPassport,
+      this.seriesPassport,
+      this.email});
+}
